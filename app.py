915edd96f69ade375bdbb427c2c2e1ca9eccad49
xianyingkong <xykong@ucsd.edu>
2024-02-28 20:13:09 -0800

    complete app

--- app.py
+++ app.py
@@ -1,26 +1,50 @@
-from flask import Flask, render_template, request
-import os
+from flask import Flask, render_template, request, jsonify
+from skimage import io
+from pyxelate import Pyx
+import base64
+import io as sysio
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
-# @app.route("/generate", methods=['POST'])
-# def generate_response():
-#     print(request.form)
-#     custom_webpath = request.form['custom_webpath']
-#     question = str(request.form['question'])
+@app.route("/generate", methods=['POST'])
+def generate_response():
+    print(request.files)
 
-#     print("### Initializing environment and creating model...")
-#     model = initialize_env_and_model()
-#     print("### Parsing webpath...")
-#     model([custom_webpath], use_strainer=True, strainer_class='s-lib-box-content')
-#     print(f"### Generating response to question: {question}")
-#     answer = model.ask(question)
+    img_file = request.files['image']
 
-#     return render_template('index.html', output=answer)
+    if img_file:
+        print("### Reading image...")
+
+        # Original image
+        original_img = io.imread(img_file)
+        
+        img_bytes = sysio.BytesIO()
+        io.imsave(img_bytes, original_img, format='PNG')
+        img_bytes.seek(0)
+        base64_image_ori = base64.b64encode(img_bytes.getvalue()).decode('utf-8')
+
+        print("### Making image...")
+        # Pixelated image
+        downsample=10
+        palette=10
+        pixel_img_bytes = sysio.BytesIO()
+        pyx = Pyx(factor=downsample, palette=palette, dither='none').fit_transform(original_img)
+        io.imsave(pixel_img_bytes, pyx, format='PNG')
+        pixel_img_bytes.seek(0)
+        base64_image_pix = base64.b64encode(pixel_img_bytes.getvalue()).decode('utf-8')
+
+        # Prepare response
+        response = {
+            'base64_image_original': base64_image_ori,
+            'base64_image_pixelated': base64_image_pix
+        }
+
+        return jsonify(response)
+
 
 if __name__ == "__main__":
     app.run()
